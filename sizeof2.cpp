e323c2c7eb82594f57076d67658555ab36aad68c
sherlynt <tiwarisherlyn@gmail.com>
2024-08-07 10:41:00 +0530

    Create sizeof2.cpp

---
+++ sizeof2.cpp
@@ -0,0 +1,24 @@
+//Sherlyn Tiwari 
+//Prn: 23070123119
+//exp 2
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Enter any integer: ";
+    cin>>a;
+    cout<<"\n Integer = "<<a<<" and size is "<<sizeof(a)<<" bytes";
+    
+    float b;
+    cout<<"\n Enter a number: ";
+    cin>>b;
+    cout<<"\n Float= "<<b<<" and size is "<<sizeof(b)<<" bytes";
+    
+    short int c;
+    cout<<"\n Enter an interger: ";
+    cin>>c;
+    cout<<"\n Short integer = "<<c<<" and size is "<<sizeof(c)<<" bytes";
+    return 0;
+}
