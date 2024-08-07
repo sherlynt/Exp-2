cb11bdac0c6624a2eea8e2c745fafd0db42f1259
sherlynt <tiwarisherlyn@gmail.com>
2024-08-07 10:41:40 +0530

    Create sizeof3.cpp

---
+++ sizeof3.cpp
@@ -0,0 +1,24 @@
+//Sherlyn Tiwari
+//Prn: 23070123119
+//exp 2
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Enter any integer:";
+    cin>>a;
+    cout<<"\nInteger= "<<a<<"and size is "<<sizeof(a)<<"bytes";
+
+    int b;  
+    cout<<"\nEnter a number:";
+    cin>>b;
+    cout<<"\nREGISTER="<<b<<"and size is"<<sizeof(b)<<"bytes";
+
+    int c;  
+    cout<<"\nEnter an integer: ";
+    cin>>c;
+    cout<<"\nAuto="<<c<<"and size is"<<sizeof(c)<<"bytes";
+
+    return 0;
+}
